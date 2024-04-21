b5d18fd1fc893b603f38a7878550e084a890d5d5
bobjoan <bobjoan966@gmail.com>
2024-04-21 17:52:46 +0100

    updated page

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 let inputEl = document.getElementById("input-el");
 let buttonEl = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 buttonEl.addEventListener("click", function () {
   myLeads.push(inputEl.value); // push input to array
+  inputEl.value = ""; // refreshes page
+  renderLeads();
 });
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"; // appends a new list item to the HTML content inside the ul element, using the value from the myLeads array at index i.
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>
+    <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+    </li>`;
+    ulEl.innerHTML = listItems;
+  }
 }
